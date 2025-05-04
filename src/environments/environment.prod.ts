a7047b421b84c8d9067e786865fb38d91355a1c0
Manmeet Devgun <manmeet.devgun@nagarro.com>
2025-05-04 21:26:25 +0530

    update .env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://kitchensink-backend-b9ame2g6bddnaee7.centralindia-01.azurewebsites.net/api'
+  apiUrl: 'https://kitchensink-backend-b9ame2g6bddnaee7.centralindia-01.azurewebsites.net'
 };
